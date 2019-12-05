5f169f0bfa37e8dd1e94c428d5677ca3a543f3ea
Kapil Agrawal <kapil.agrawal2204@gmail.com>
2019-12-05 16:11:36 -0600

    updating exercise

--- exercises/exercise-1.py
+++ exercises/exercise-1.py
@@ -1,6 +1,6 @@
-# Objective : Modify example-1e.py to print ASN <=> AS-NAME mapping for ANY list of AS numbers
-            # Store the output in a Python dictionary
+# Objective : Modify example-1e.py to store mapping of any given ASN <=> AS-NAME in a Python dictionary 
 
 # example : your input should look something like this 
-# ./exercise-1.py 1224 38 19782 1767
+# INPUT : ./exercise-1.py 1224 38 19782 1767
+# OUTPUT : {'1224': 'National Center for Supercomputing Applications', '38': 'University of Illinois', '19782': 'Indiana University', '1767': 'Indiana Highe# r Education Telecommunication System'} 
 
